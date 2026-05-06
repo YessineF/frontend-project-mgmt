423f939251095e059e3f5a0ec422c6ec885f7e17
Yessine Faires <fairesyessine@gmail.com>
2026-05-06 18:33:31 +0100

    fix: chemin complet dans les services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-project-mgmt-production.up.railway.app/api'
+  apiUrl: 'https://backend-project-mgmt-production.up.railway.app'
 };
